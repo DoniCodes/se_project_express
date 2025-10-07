64bab5b79c77506ba3e1d69e5491bf03f07d423a
DoniCodes <adonis.santos02@icloud.com>
2025-10-07 15:11:02 -0400

    Add required config for this project. Make file structure with the starting files. Make app.js an express app and connect to MongoDB

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { PORT = 3001 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+
+const app = express();
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
