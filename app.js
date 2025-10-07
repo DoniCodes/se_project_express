045f063bc0118e5336f0eb457c48e954551643c6
DoniCodes <adonis.santos02@icloud.com>
2025-10-07 15:44:23 -0400

    Setup user routes

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { PORT = 3001 } = process.env;
-
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+const mainRouter = require('./routes/index');
 
 const app = express();
+const { PORT = 3001 } = process.env;
+
+app.use("/", mainRouter);
+
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db')
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
