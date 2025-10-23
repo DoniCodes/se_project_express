7564496d907c9cb8a2c269263cf12a9ae1f059f6
DoniCodes <adonis.santos02@icloud.com>
2025-10-23 18:19:38 -0400

    Create getUser controller to get user by id. Add error handling. Add controller to GET route that gets user by id

--- controllers/users.js
+++ controllers/users.js
@@ -24,7 +24,26 @@ const createUser = (req, res) => {
     });
 }
 
+const getUser = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+  .orFail()
+    .then((user) =>  res.status(200).send(user))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === 'DocumentNotFoundError') {
+        return res.status(404).send({ message: 'User not found' });
+      } else if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Invalid user ID' });
+
+    }
+      res.status(500).send({ message: err.message });
+    });
+}
+
 module.exports = {
   getUsers,
-  createUser
+  createUser,
+  getUser,
 };
