b54c015d9483a826615dcbd6b7f5f64a550ec470
DoniCodes <adonis.santos02@icloud.com>
2025-10-23 17:19:56 -0400

    Make getUsers and createUsers controllers and add the controllers to the get and post routers

---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send({ message: err.message });
+      });
+}
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    });
+}
+
+module.exports = {
+  getUsers,
+  createUser
+};
