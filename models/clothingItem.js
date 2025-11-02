d2431bf90657b04355447d41d8384034d95c08cb
DoniCodes <adonis.santos02@icloud.com>
2025-11-02 17:29:19 -0500

    Update clothing schema with required fields

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -5,14 +5,30 @@ const clothingItem = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   weather: {
     type: String,
     required: true,
+    enum: ['hot', 'warm', 'cold'],
   },
   imageUrl: {
       type: String,
       required: true,
+      owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        default: [],
+      }],
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
       validate: {
         validator: (v) => validator.isURL(v),
        message: 'Link is not valid',
